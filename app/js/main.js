ec374eec0f50d4cfb2503e466c884afe6f6ace2f
Andrey <shock.work@mail.ru>
2020-11-10 15:30:42 +0300

    mobile menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,20 @@
-$(function(){
+let burger  = document.querySelector('.burger'),
+    menu    = document.querySelector('.mobile-menu'),
+    close   = document.querySelector('.close')
 
+if (menu.classList.contains('active')) {
+    document.body.classList.add('lock')
+} else {
+    document.body.classList.remove('lock')
+}
 
+burger.addEventListener('click', () => {
+    document.body.classList.add('lock')
+    menu.classList.add('active')
+})
 
-  
-});
+close.addEventListener('click', () => {
+    document.body.classList.remove('lock')
+    menu.classList.remove('active')
+})
+
